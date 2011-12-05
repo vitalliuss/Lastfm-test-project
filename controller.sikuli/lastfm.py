8ada082e62e77b9a88f2b97a345ebf90b3d56523
unknown <admin@hp-probook.(none)>
2011-12-05 23:50:32 +0300

    First test case has been updated. Framework is ready to be filled with tests.

--- controller.sikuli/lastfm.py
+++ controller.sikuli/lastfm.py
@@ -9,23 +9,47 @@ class Lastfm(ScenarioBase):
 		self.variables = variables
 
 	def firefoxIsOpenState(self, args):
+		path = args["Path"]
+		self.log.info("Path to Firefox: "+path)
 		self.closeFirefox(args)
-		App.open("c:\\Program Files (x86)\\Mozilla Firefox\\Firefox.exe")
+		App.open(path)
 		#wait Firefox to be started. Should be replaced by pretty "wait condition"
 		wait(5)
-		
+		self.maximizeActiveWindow()
+
+	def maximizeActiveWindow(self):
+		if exists("Window_Maximize_Classic.png"):
+			click("Window_Maximize_Classic.png")
+		#Not implemented yet
+		#if exists("Window_Maximize_Xp.png"):
+		#	click("Window_Maximize_Xp.png")
+		#Not implemented yet
+		#if exists("Window_Maximize_Seven.png"):
+		#	click("Window_Maximize_Seven.png")
+
 	def closeFirefox(self, agrs):
 		myApp = App("Firefox")
 		myApp.close()
-		
-	def openLastFm(self, args):	
+
+	def openLastFm(self, args):
+		time = args["Time"]
 		type("l", KEY_CTRL) # switch to address field
 		self.set("www.last.fm")
-		wait("Lastfm_logo.png", 10)
-		if exists("Lastfm_logo.png"):
+		wait("Lastfm_logo.png", int(time))
+		if self.mainPageOpened():
 			self.log.info("Last Fm main page opened")
 		else:
-			self.log.info("Last Fm main page not opened in 10 seconds")
+			self.log.info("Last Fm main page not opened in "+time+"seconds")
+
+	def LastFmIsOpenedState(self, args):
+		if not self.mainPageOpened():
+			self.openLastFm()
+
+	def mainPageOpened(self):
+		if exists("Lastfm_logo.png"):
+			return True
+		else:
+			return False
 
 	def loggedIn(self):
 		if exists("Lastfm_inbox_logout.png"):
@@ -41,7 +65,7 @@ class Lastfm(ScenarioBase):
 			return False
 
 	def verifyICanSeeMainPage(self, args):
-		if exists("Lastfm_logo.png"):
+		if self.mainPageOpened():
 			self.log.info("I can see main page")
 		else:
 			self.log.error("I can not see main page")
